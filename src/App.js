edd54bb0e040b726655ee200ce86c1e2c7686bd4
huuhungiu17031 <nguyenhuuhung.9a10.2013@gmail.com>
2024-03-14 23:01:35 +0700

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { RouteInterface, publicRoutes } from '~/routes'
+import { publicRoutes } from '~/routes'
 import { Fragment } from 'react'
-import DefaultLayout from '~/layouts/default';
+import DefaultLayout from '~/layouts/default'
 
 function App() {
-    return <Router>
-        <div className='App'>
-            <Routes>
-                {publicRoutes.map((route, index) => {
-                    const Page = route.component;
-                    let Layout = DefaultLayout;
-                    if (route.layout) {
-                        Layout = route.layout;
-                    } else if (route.layout === null) {
-                        Layout = Fragment;
-                    }
-                    return <Route key={index} path={route.path} element={<Layout><Page /></Layout>} />
-                })}
-            </Routes>
-        </div>
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            const Page = route.component
+            let Layout = DefaultLayout
+            if (route.layout) {
+              Layout = route.layout
+            } else if (route.layout === null) {
+              Layout = Fragment
+            }
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            )
+          })}
+        </Routes>
+      </div>
     </Router>
+  )
 }
 
 export default App
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,7 +5,7 @@ import React from 'react'
 
 function HomePage() {
     return <div>
-        <h1>HomePage</h1>
+        <h1>HomePage Hello</h1>
     </div>
 
 }
